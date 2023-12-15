762e234959c41cf1ce554e20c83fc10454a30b4b
MonishaDM <dmoni0709@gmail.com>
2023-12-15 12:59:13 +0530

    commit 1

--- src/main/java/com/max/ObjectRepo/StoreLocatorPage.java
+++ src/main/java/com/max/ObjectRepo/StoreLocatorPage.java
@@ -1,5 +1,7 @@
 package com.max.ObjectRepo;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import io.appium.java_client.android.AndroidDriver;
@@ -7,11 +9,36 @@ import io.appium.java_client.android.AndroidDriver;
 public class StoreLocatorPage {
 
 	AndroidDriver driver;
+	
+	@FindBy(id="com.applications.max:id/favSearch")
+	private WebElement searchButton;
+	
+	@FindBy(xpath = "//android.widget.ImageView[@resource-id='com.applications.max:id/imageFavRemove']")
+	private WebElement closeButton;
+	
+	@FindBy(xpath = "//android.widget.TextView[@text='ADD TO BASKET']")
+	private WebElement addToBasket;
+	
+	@FindBy(xpath = "//android.widget.Spinner[@resource-id='com.applications.max:id/spinnerSize']")
+	private WebElement selectdropdown;
+	
+	@FindBy(xpath = "//android.widget.CheckedTextView[@text='XS']")
+	private WebElement selectSize;
+	
+	@FindBy(xpath = "//android.widget.TextView[@text='REMOVE ALL']")
+	private WebElement removeAll;
+	
+	@FindBy(id="android:id/button1")
+	private WebElement OKButton;
+	
+	@FindBy(id="android:id/button2")
+	private WebElement cancelButton;
+	
 	public StoreLocatorPage() {
 		PageFactory.initElements( driver, this);
 	}
 	
 	public void store() {
-
+		
 	}
 }
